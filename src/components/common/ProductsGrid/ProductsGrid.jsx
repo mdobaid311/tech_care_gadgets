4d63e9c84676bd0c94c41d424cb75396aa3c2a5f
mdobaid311 <mdobaid311@gmail.com>
2023-05-27 15:45:46 +0530

    new commit

--- src/components/common/ProductsGrid/ProductsGrid.jsx
+++ src/components/common/ProductsGrid/ProductsGrid.jsx
@@ -6,29 +6,31 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { client } from "../../../sanity/client";
 
-const ProductsGrid = () => {
-  const [products, setProducts] = useState(null);
+const ProductsGrid = ({ products }) => {
+  // const [products, setProducts] = useState(null);
 
-  useEffect(() => {
-    client
-      .fetch(
-        `*[_type == "product"]{_id,name, price, category,"imageUrl": image[].asset->url
-    }`
-      )
-      .then((products) => {
-        setProducts(products);
-        console.log(products);
-      })
-      .catch(console.error);
-  }, []);
+  // useEffect(() => {
+  //   client
+  //     .fetch(
+  //       `*[_type == "product"]{_id,name, price, category,"imageUrl": image[].asset->url
+  //   }`
+  //     )
+  //     .then((products) => {
+  //       setProducts(products);
+  //       // console.log(products);
+  //     })
+  //     .catch(console.error);
+  // }, []);
+
+  console.log(products)
 
   return (
     <div className="products__grid">
       <div className="products">
         {products?.map((product) => {
           return (
-            <Link key={product._id} to={`/shop/${product._id}`}>
-              <Product product={product} key={product.id} />
+            <Link key={product?._id} to={`/shop/${product?._id}`}>
+              <Product product={product} key={product?.id} />
             </Link>
           );
         })}
--- src/components/common/ShopPage/ProductsSection/ProductsSection.jsx
+++ src/components/common/ShopPage/ProductsSection/ProductsSection.jsx
@@ -1,151 +1,148 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./productsSection.scss";
 import ProductsGrid from "../../ProductsGrid/ProductsGrid";
+import { client } from "../../../../sanity/client";
 
 const ProductsSection = () => {
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [brandFilter, setBrandFilter] = useState([]);
+  const [brands, setBrands] = useState([]);
+
+  useEffect(() => {
+    client
+      .fetch(`*[_type == "product"]{ brand }`)
+      .then((result) => {
+        const uniqueBrands = [
+          ...new Set(result.map((product) => product.brand)),
+        ];
+        setBrands(uniqueBrands);
+        console.log(uniqueBrands);
+      })
+      .catch(console.error);
+
+    client
+      .fetch(
+        `*[_type == "product"]{_id,name, price, category,"imageUrl": image[].asset->url}`
+      )
+      .then((products) => {
+        setProducts(products);
+      })
+      .catch(console.error);
+  }, []);
+
+  const filterCategory = (categories) => {
+    console.log(
+      `*[_type == 'product' && category in ["${categories.join(
+        '", "'
+      )}"]]{_id,name, price, category,"imageUrl": image[].asset->url}`
+    );
+    client
+      .fetch(
+        `*[_type == 'product' && category in ["${categories.join(
+          '", "'
+        )}"]]{_id,name, price, category,"imageUrl": image[].asset->url}
+      `
+      )
+      .then((result) => {
+        setProducts(result);
+        console.log(result);
+      })
+      .catch(console.error);
+  };
+
+  const filterBrand = (brand) => {
+    console.log(
+      `*[_type == 'product' && brand in ["${brand.join(
+        '", "'
+      )}"]]{_id,name, price, category,"imageUrl": image[].asset->url}`
+    );
+    client
+      .fetch(
+        `*[_type == 'product' && brand in ["${brand.join(
+          '", "'
+        )}"]]{_id,name, price, category,"imageUrl": image[].asset->url}
+      `
+      )
+      .then((result) => {
+        setProducts(result);
+        console.log(result);
+      })
+      .catch(console.error);
+  };
+
   return (
     <div className="products__section">
       <div className="choose__filter__container">
         <h3>Brand</h3>
-        <div className="filter__container">
-          <input type="checkbox" />
-          <span>ZARA</span>
-        </div>
-        <div className="filter__container">
-          <input type="checkbox" />
-          <span>GUCCI</span>
-        </div>
-        <div className="filter__container">
-          <input type="checkbox" />
-          <span>CHANEL</span>
-        </div>
-        <div className="filter__container">
-          <input type="checkbox" />
-          <span>LV</span>
-        </div>
-        <div className="filter__container">
-          <input type="checkbox" />
-          <span>DIOR</span>
-        </div>
-        <div className="filter__container">
-          <input type="checkbox" />
-          <span>Trending</span>
-        </div>
-        <h3>SIZES</h3>
-        <div className="sizes__container">
-          <div className="size__box selected__size">XS</div>
-          <div className="size__box">S</div>
-          <div className="size__box">M</div>
-          <div className="size__box">L</div>
-          <div className="size__box">XL</div>
-          <div className="size__box">XXL</div>
-        </div>
-        <h3>Color</h3>
-        <div className="colors__container">
-          <div className="color__box selected__color">
-            <div
-              className="color__box__inner"
-              style={{
-                backgroundColor:
-                  "#" +
-                  ((Math.random() * 0xffffff) << 0)
-                    .toString(16)
-                    .padStart(6, "0"),
-              }}
-            ></div>
-          </div>
-          <div className="color__box ">
-            <div
-              className="color__box__inner"
-              style={{
-                backgroundColor:
-                  "#" +
-                  ((Math.random() * 0xffffff) << 0)
-                    .toString(16)
-                    .padStart(6, "0"),
-              }}
-            ></div>
-          </div>
-          <div className="color__box ">
-            <div
-              className="color__box__inner"
-              style={{
-                backgroundColor:
-                  "#" +
-                  ((Math.random() * 0xffffff) << 0)
-                    .toString(16)
-                    .padStart(6, "0"),
-              }}
-            ></div>
-          </div>
-          <div className="color__box ">
-            <div
-              className="color__box__inner"
-              style={{
-                backgroundColor:
-                  "#" +
-                  ((Math.random() * 0xffffff) << 0)
-                    .toString(16)
-                    .padStart(6, "0"),
-              }}
-            ></div>
-          </div>
-          <div className="color__box ">
-            <div
-              className="color__box__inner"
-              style={{
-                backgroundColor:
-                  "#" +
-                  ((Math.random() * 0xffffff) << 0)
-                    .toString(16)
-                    .padStart(6, "0"),
-              }}
-            ></div>
-          </div>
-          <div className="color__box ">
-            <div
-              className="color__box__inner"
-              style={{
-                backgroundColor:
-                  "#" +
-                  ((Math.random() * 0xffffff) << 0)
-                    .toString(16)
-                    .padStart(6, "0"),
-              }}
-            ></div>
-          </div>
-          <div className="color__box ">
-            <div
-              className="color__box__inner"
-              style={{
-                backgroundColor:
-                  "#" +
-                  ((Math.random() * 0xffffff) << 0)
-                    .toString(16)
-                    .padStart(6, "0"),
-              }}
-            ></div>
-          </div>
-          <div className="color__box ">
-            <div
-              className="color__box__inner"
-              style={{
-                backgroundColor:
-                  "#" +
-                  ((Math.random() * 0xffffff) << 0)
-                    .toString(16)
-                    .padStart(6, "0"),
-              }}
-            ></div>
-          </div>
-        </div>
-        <h3>Price Range</h3>
-        <div className="price__range__container">
-          <input type="range" />
-        </div>
-      </div>
 
-      <ProductsGrid />
+        {brands?.map((product) => {
+          return (
+            <div className="filter__container" key={product}>
+              <input
+                type="checkbox"
+                onChange={(e) => {
+                  console.log(e.target.checked);
+                  if (e.target.checked) {
+                    setBrandFilter((prev) => [...prev, product]);
+                    filterBrand([...brandFilter, product]);
+                  } else {
+                    setBrandFilter((prev) =>
+                      prev.filter((cat) => cat !== product)
+                    );
+                    filterBrand(brandFilter.filter((cat) => cat !== product));
+                  }
+                }}
+              />
+              <span>{product}</span>
+            </div>
+          );
+        })}
+        <h3>Category</h3>
+        <div className="filter__container">
+          <input
+            type="checkbox"
+            onChange={(e) => {
+              console.log(e.target.checked);
+              if (e.target.checked) {
+                setCategories((prev) => [...prev, "mobiles"]);
+                filterCategory([...categories, "mobiles"]);
+              } else {
+                setCategories((prev) =>
+                  prev.filter((cat) => cat !== "mobiles")
+                );
+                filterCategory(categories.filter((cat) => cat !== "mobiles"));
+              }
+            }}
+          />
+          <span>Mobile</span>
+        </div>
+        <div className="filter__container">
+          <input
+            type="checkbox"
+            onChange={(e) => {
+              console.log(e.target.checked);
+              if (e.target.checked) {
+                setCategories((prev) => [...prev, "acessories"]);
+                filterCategory([...categories, "acessories"]);
+              } else {
+                setCategories((prev) =>
+                  prev.filter((cat) => cat !== "acessories")
+                );
+                filterCategory(
+                  categories.filter((cat) => cat !== "acessories")
+                );
+              }
+            }}
+          />
+          <span>Acessories</span>
+        </div>
+        {/* <h3>Price Range</h3> */}
+        {/* <div className="price__range__container">
+          <input type="range" />
+        </div> */}
+      </div>
+      <ProductsGrid products={products} />
     </div>
   );
 };
