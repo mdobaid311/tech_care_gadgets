05aa9b1100ceea292b389a9be5920eb82b743ba7
mdobaid311 <mdobaid311@gmail.com>
2023-05-27 14:59:22 +0530

    new commit

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -10,23 +10,32 @@ const Contact = () => {
       <h1>Contact Us</h1>
       <div className="contact__container">
         <div className="contact__container__left">
-          <div className="contact">
+          <div className="contact_info">
             <div className="icon">
               <BsFillTelephoneFill />
             </div>
-            1234 Main St. Anywhere, ST 12345
+            <h4>
+              Address
+              <span>1234 Main St. Anywhere, ST 12345</span>
+            </h4>
           </div>
-          <div className="contact">
+          <div className="contact_info">
             <div className="icon">
               <BsFillTelephoneFill />
             </div>
-            9515281551
+            <h4>
+              Phone
+              <span>9515281551</span>
+            </h4>
           </div>
-          <div className="contact">
+          <div className="contact_info">
             <div className="icon">
               <AiOutlineMail />
             </div>
-            mdobaid311@gmail.com
+            <h4>
+              Email
+              <span>mdobaid311@gmail.com</span>
+            </h4>
           </div>
         </div>
         <div className="contact__container__right">
@@ -38,6 +47,14 @@ const Contact = () => {
             <button>Send</button>
           </form>
         </div>
+      </div>
+      <div className="location__container">
+        <h1>FIND US ON MAP</h1>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2867.8730873887343!2d-79.477479!3d44.044677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882ad1205a45e7f3%3A0x9bb02aca1778d240!2sTech%20Care%20Gadgets!5e0!3m2!1sen!2sin!4v1685179314614!5m2!1sen!2sin"
+          loading="lazy"
+          className="map"
+        ></iframe>
       </div>
     </div>
   );
