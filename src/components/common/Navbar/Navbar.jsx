a732e07aec1d8ab6db9b6e9d3a519ee470763d07
mdobaid311 <mdobaid311@gmail.com>
2023-06-02 12:32:17 +0530

    new commit

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -9,94 +9,100 @@ import { useStateContext } from "../../../context/stateContext";
 import Menu from "../Menu/Menu";
 
 const Navbar = () => {
-  const { user, totalPrice: cartTotal } = useStateContext();
-  const [isOpen, setIsOpen] = useState(false);
+  const [isMenuActive, setIsMenuActive] = useState(false);
 
-  const toggleNavbar = () => {
-    setIsOpen(!isOpen);
+  const toggleMenu = () => {
+    setIsMenuActive(!isMenuActive);
+  };
+
+  const toggleDropdown = (event) => {
+    const toggleButton = event.currentTarget;
+    toggleButton.querySelector("svg").classList.toggle("up");
+    toggleButton.nextElementSibling.classList.toggle("open");
   };
 
   return (
-    <>
-      <div className="navbar">
-        <div className="navbar__left">
-          <img src={logo} alt="" className="navbar__logo" />
-        </div>
-        <div className="navbar__center">
-          <ul className="navbar__menu">
-            <li className="navbar__menu-item">
-              <Link to="/ ">Home</Link>
+    <header>
+      <nav className="navbar container">
+        <a href="#">
+          <img src={logo} alt="Tech Care Gadgets" className="navbar__logo" />
+        </a>
+        <div className={`navbar__menu ${isMenuActive ? "right-open" : ""}`}>
+          <ul className="navbar__list">
+            <li className="navbar__list-drop" onClick={toggleDropdown}>
+              <a href="#">
+                Features
+                <svg
+                  className="toggle-button"
+                  stroke="#686868"
+                  strokeWidth="1.5"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 10 10"
+                >
+                  <path d="M1 1l4 4 4-4" />
+                </svg>
+              </a>
+              <ul className="navbar__list-drop-menu features">
+                <li>
+                  <a href="#">
+                    <span>Todo List</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">Calendar</a>
+                </li>
+                <li>
+                  <a href="#">Reminders</a>
+                </li>
+                <li>
+                  <a href="#">Planning</a>
+                </li>
+              </ul>
             </li>
-            <li className="navbar__menu-item shop">
-              <Link to="/shop">Shop</Link>
-              <div className="menu__box">
-                <Menu />
-              </div>
+            <li className="navbar__list-drop" onClick={toggleDropdown}>
+              <a href="#">Company</a>
+              <ul className="navbar__list-drop-menu company">
+                <li>
+                  <a href="#">History</a>
+                </li>
+                <li>
+                  <a href="#">Our Team</a>
+                </li>
+                <li>
+                  <a href="#">Blog</a>
+                </li>
+              </ul>
             </li>
-            <li className="navbar__menu-item">
-              <Link to="/contact">Contact Us</Link>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+            <li>
+              <a href="#">About</a>
             </li>
           </ul>
-          <div className="search">
-            <input type="text" placeholder="Search" />
-            <BiSearch className="icon" color="#fff" />
-            {/* <span>Search</span> */}
+          <div className="navbar__buttons">
+            <a className="navbar__buttons-login" href="#">
+              Login
+            </a>
+            <button className="navbar__buttons-register" href="#">
+              Register
+            </button>
           </div>
         </div>
-        <div className="navbar__right">
-          <ul className="navbar__menu">
-            {!user ? (
-              <>
-                <li className="navbar__menu-item">
-                  <Link to="/login">Sign in</Link>
-                </li>
-                <li className="navbar__menu-item">
-                  <Link to="/signup">Create an account</Link>
-                </li>
-              </>
-            ) : (
-              <li className="navbar__menu-item">
-                <Link to="/">{user.name}</Link>
-              </li>
-            )}
-          </ul>
-          <Link to="#">
-            <AiOutlineHeart className="icon" />
-          </Link>
-          <Link to="#">
-            <BiShoppingBag className="icon" />
-          </Link>
-          <Link to="/cart" className="navbar__shopping_cart">
-            <span>Shopping Cart</span>
-            <span>{cartTotal} USD</span>
-          </Link>
+        <div className="demo">
+          <div
+            id="menu-icon"
+            className={`menu ${isMenuActive ? "active" : ""}`}
+            onClick={toggleMenu}
+          >
+            <span className="bar1"></span>
+            <span className="bar2"></span>
+            <span className="bar3"></span>
+          </div>
         </div>
-      </div>
-      <nav className="mobile__navbar">
-        <div className="logo">
-          <img src={logo} alt="Logo Image" />
-        </div>
-        <div
-          className={`hamburger ${isOpen ? "toggle" : ""}`}
-          onClick={toggleNavbar}
-        >
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
-        </div>
-        <ul className={`nav-links ${isOpen ? "open" : ""}`}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/shop">Shop</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact us</Link>
-          </li>
-        </ul>
       </nav>
-    </>
+    </header>
   );
 };
 
