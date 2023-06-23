659f6e4bd987913785867e351fc9f4328992540c
mdobaid311 <mdobaid311@gmail.com>
2023-06-24 02:27:25 +0530

    new commit

--- src/components/common/HomePage/BrandLogoRibbon/BrandLogoRibbon.jsx
+++ src/components/common/HomePage/BrandLogoRibbon/BrandLogoRibbon.jsx
@@ -5,7 +5,7 @@ import image1 from "../../../../assets/brands/Apple-Logo.png";
 import image2 from "../../../../assets/brands/OnePlus-Logo.png";
 import image3 from "../../../../assets/brands/Sony-logo.png";
 import image4 from "../../../../assets/brands/samsung.png";
-import image5 from "../../../../assets/brands/nokia.webp";
+import image5 from "../../../../assets/brands/lg.png";
 import image6 from "../../../../assets/brands/marshall.png";
 
 const BrandLogoRibbon = () => {
