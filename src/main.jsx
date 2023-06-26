405d1defb18e6ae8ec817e0ae59c45d0389ff562
mdobaid311 <mdobaid311@gmail.com>
2023-06-26 14:32:36 +0530

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import App from "./App.jsx";
 import "./index.scss";
 import { StateContext } from "./context/stateContext.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider 
-    clientId="889079172962-nbe5vlib8pr5uemsi0jh7t5l6qnscnjd.apps.googleusercontent.com"
-  >
+  <GoogleOAuthProvider clientId="889079172962-nbe5vlib8pr5uemsi0jh7t5l6qnscnjd.apps.googleusercontent.com">
     <StateContext>
       <App />
+      <Analytics />
     </StateContext>
   </GoogleOAuthProvider>
 );
